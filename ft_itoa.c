65daec2332a629e549c098cf0ee1203ef20837c4
Simon MAGDELAINE <smagdela@student.42.fr>
2021-05-26 16:17:18 +0200

    itoa Working and Normed

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: smagdela <smagdela@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/25 14:47:50 by smagdela          #+#    #+#             */
-/*   Updated: 2021/05/26 14:57:03 by smagdela         ###   ########.fr       */
+/*   Updated: 2021/05/26 16:16:28 by smagdela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,27 +46,27 @@ static int	ft_pow(int nb, int power)
 
 char	*ft_itoa(int n)
 {
-	char	*str;
-	int		tenth_pow;
-	int		i;
+	char			*str;
+	int				tenth_pow;
+	int				i;
+	unsigned int	tmp_n;
 
 	tenth_pow = ft_nblen(n);
 	str = (char *)malloc(tenth_pow + 1);
 	if (!str)
 		return (NULL);
-	if (n == -2147483648)
-		return ("-2147483648");
 	i = 0;
+	tmp_n = n;
 	if (n < 0)
 	{
 		str[i++] = '-';
 		--tenth_pow;
-		n = -n;
+		tmp_n = -n;
 	}
 	while (tenth_pow--)
 	{
-		str[i++] = (n / ft_pow(10, tenth_pow)) + '0';
-		n = n % ft_pow(10, tenth_pow);
+		str[i++] = (tmp_n / ft_pow(10, tenth_pow)) + '0';
+		tmp_n = tmp_n % ft_pow(10, tenth_pow);
 	}
 	str[i] = '\0';
 	return (str);
